d12c533991a873bd2869945d5b0904c8812d8094
KusalNayanajith99 <kusalnayanajith88@gmail.com>
2023-08-14 18:50:49 +0530

    Initial Commit

---
+++ API/Program.cs
@@ -0,0 +1,32 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<Infrastructure.Data.StoreContext>(x => x.UseSqlite(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+ 
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
